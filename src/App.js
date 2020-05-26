c23279f00cf19c8bd15dc96060e47369da9cb739
tsaristbomba <andrexerdna@gmail.com>
2020-05-26 11:59:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Pulse from 'react-reveal/Pulse'
+
+import './App.css'
+
+import { imgs } from './Images/Images'
 
 function App() {
+  const [bgImage, setBg] = useState('')
+  const [card, setCard] = useState('') 
+
+  useEffect(() => {
+    fetchQuote()
+    fetchImage()
+  }, [])
+
+  const fetchQuote = () => {
+    axios.get('https://api.stoic.rest/quote')
+      .then(res => {
+        setCard('')
+        const { author, quote } = res.data
+        const authorCaps = author.charAt(0).toUpperCase() + author.slice(1)
+        setCard(
+          <Pulse duration={500}>
+            <div className="inner-container">
+              <p> {quote} </p>
+              <cite> {authorCaps} </cite>
+              <button onClick={handleOther}>Load Another</button>
+            </div>
+          </Pulse>
+        )
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      
+  }
+
+  const fetchImage = () => {
+    const index = Math.floor(Math.random() * 10)
+    setBg(imgs[index])
+  }
+
+  const handleOther = () => {
+    fetchQuote()
+    fetchImage()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="image-container" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${bgImage})` }}>
+      {card}
     </div>
   );
 }
